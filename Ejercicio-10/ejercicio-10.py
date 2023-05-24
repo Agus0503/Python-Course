1e7686672690766142c3d7a1f1ed765713654f78
Agus <107086899+Agus0503@users.noreply.github.com>
2023-05-24 00:32:04 -0300

    Update ejercicio-10.py

--- Ejercicio-10/ejercicio-10.py
+++ Ejercicio-10/ejercicio-10.py
@@ -10,15 +10,16 @@ from tkinter import ttk
 
 window = tkinter.Tk()
 
-'''RADIOBUTTON'''
-
 selected = tkinter.StringVar()
-r1 = ttk.Radiobutton(window,text='Si',value='1',variable=selected)
-r2 = ttk.Radiobutton(window,text='No',value='2',variable=selected)
-r3 = ttk.Radiobutton(window,text='Nose',value='3',variable=selected)
 
-r1.grid(column=0,row=1,padx=5,pady=5)
-r2.grid(column=0,row=2,padx=5,pady=5)
-r3.grid(column=0,row=3,padx=5,pady=5)
+r1 = ttk.Radiobutton(window, text='Si', value='1', variable=selected)
+r2 = ttk.Radiobutton(window, text='No', value='2', variable=selected)
+r3 = ttk.Radiobutton(window, text='Nose', value='3', variable=selected)
+reset = ttk.Button(window, text='Reset', command=lambda: selected.set(""))
+
+r1.grid(column=0, row=1, padx=5, pady=5)
+r2.grid(column=0, row=2, padx=5, pady=5)
+r3.grid(column=0, row=3, padx=5, pady=5)
+reset.grid(column=0, row=4, padx=5, pady=5)
 
 window.mainloop()
